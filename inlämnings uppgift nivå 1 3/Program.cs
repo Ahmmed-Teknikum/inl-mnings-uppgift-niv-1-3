20587c46fa15fc7812b2fa1c774bd5e251503336
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-13 08:26:07 +0100

    Add project files.

---
+++ inlämnings uppgift nivå 1 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace inlämningUppgift_._nivå1_._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            int tal= int.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine("Skriv in ett annat heltal");
+            int tal1= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ett annat heltal");
+            int tal2= int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Skriv in ett annat heltal");
+            int tal3= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ett annat heltal");
+            int tal4= int.Parse(Console.ReadLine());
+
+            int sum= tal+tal1+ tal2+ tal3+ tal4;
+            Console.WriteLine("Summan blir "  +  sum);
+
+
+        }
+    }
+}
